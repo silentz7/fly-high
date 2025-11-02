8603ec2d2a07cc8e96bf37f7803926f3b67338f6
silentz7 <gamesahi42@gmail.com>
2025-11-02 23:35:12 +0530

    Update game.js

--- game/game.js
+++ game/game.js
@@ -153,7 +153,7 @@ function resetGame() {
     poleX += 400;
   }
 
-  greenLine.x = poles[poles.length - 1].x + 150;
+  greenLine.x = poles[poles.length - 1].x + 50;
   popup.classList.add("hidden");
   loseSound.pause();
   winSound.pause();
@@ -224,6 +224,7 @@ if (!gameWin && bird.x + bird.width >= greenLine.x) {
 }
 
 update();
+
 
 
 
